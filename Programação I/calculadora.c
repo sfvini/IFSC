ac72f0a65a1d3e37421ede68a40da112ed1c0ac9
sfvini <viniciusicmsc@gmail.com>
2025-05-30 11:19:19 -0300

    C

--- Programação I/calculadora.c
+++ Programação I/calculadora.c
@@ -1,10 +1,33 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main(){
-    
+int main()
+{
+    float num1, num2, res;
+    int op;
 
-//     printf("Escreva a operação matemática (Ex: 3 + 5): ");
-//     scanf("%f %d %f",)
+    printf("Escreva a operação matemática (Ex: 3 + 5): ");
+    scanf("%f, %d, %f", num1, op, num2);
 
-//     return 0;
-// }
+    switch (op)
+    {
+    case '+':
+        res = num1 + num2;
+        break;
+    case '-':
+        res = num1 - num2;
+        break;
+    case '*':
+        res = num1 * num2;
+        break;
+    case '/':
+        res = num1 / num2;
+        break;
+    default:
+        printf("Operação inválida");
+        break;
+    }
+
+    printf("%f", res);
+
+    return 0;
+}
