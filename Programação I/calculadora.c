f91e511db78f020f57a51696ca89e5bf1a31dc8f
Vini <viniciusicmsc@gmail.com>
2025-06-02 20:56:20 -0300

    C

--- Programação I/calculadora.c
+++ Programação I/calculadora.c
@@ -3,31 +3,37 @@
 int main()
 {
     float num1, num2, res;
-    int op;
+    char op;
 
     printf("Escreva a operação matemática (Ex: 3 + 5): ");
-    scanf("%f, %d, %f", num1, op, num2);
+    scanf("%f %c %f", &num1, &op, &num2);
 
-    switch (op)
+    if (op == '/' && num2 == 0)
     {
-    case '+':
-        res = num1 + num2;
-        break;
-    case '-':
-        res = num1 - num2;
-        break;
-    case '*':
-        res = num1 * num2;
-        break;
-    case '/':
-        res = num1 / num2;
-        break;
-    default:
-        printf("Operação inválida");
-        break;
+        printf("Divisão por zero não é permitida.");
     }
-
-    printf("%f", res);
+    else
+    {
+        switch (op)
+        {
+        case '+':
+            res = num1 + num2;
+            break;
+        case '-':
+            res = num1 - num2;
+            break;
+        case '*':
+            res = num1 * num2;
+            break;
+        case '/':
+            res = num1 / num2;
+            break;
+        default:
+            printf("Operação inválida");
+            break;
+        }
+        printf("%.2f\n", res);
+    }
 
     return 0;
 }
