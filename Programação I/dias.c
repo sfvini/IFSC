2d5448835c3bc60d210e879758aa6c6204037281
Vini <viniciusicmsc@gmail.com>
2025-06-06 17:00:08 -0300

    C

--- Programação I/dias.c
+++ Programação I/dias.c
@@ -3,47 +3,68 @@
 
 bool bissexto(int ano)
 {
-    return (ano % 4 == 0 && ano % 100 != 0 || (ano % 400 == 0));
+    return ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0);
 }
 
 int diasMes(int mes, int ano)
 {
-    int dias;
-
-    if (mes == 2)
-    {
-        bissexto(ano) ? dias = 29 :  dias = 28;
-    }
-
     switch (mes)
     {
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-        dias = 31;
-        break;
+    case 2:
+        return bissexto(ano) ? 29 : 28;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        return 30;
     default:
-        dias = 30;
-        break;
+        return 31;
     }
 }
 
-// bool dataValida(int, int, int)
-// {
-//     return (dia < 1 || dia > 31 || mes < 1 || mes > 12 || ano < 0 || mes == 2 && dia > 29);
-// }
+bool dataValida(int dia, int mes, int ano)
+{
+    return !(ano <= 0 || mes < 1 || mes > 12 || dia < 1 || dia > diasMes(mes, ano));
+}
+
+int diasTotais(int dia, int mes, int ano)
+{
+    int total = dia;
+
+    if (mes > 1)
+        total += 31;
+    if (mes > 2)
+        total += bissexto(ano) ? 29 : 28;
+    if (mes > 3)
+        total += 31;
+    if (mes > 4)
+        total += 30;
+    if (mes > 5)
+        total += 31;
+    if (mes > 6)
+        total += 30;
+    if (mes > 7)
+        total += 31;
+    if (mes > 8)
+        total += 31;
+    if (mes > 9)
+        total += 30;
+    if (mes > 10)
+        total += 31;
+    if (mes > 11)
+        total += 30;
+
+    return total;
+}
 
 int main()
 {
-
     int dia, mes, ano;
 
     printf("Digite a data (dd/mm/aaaa): ");
     scanf("%d/%d/%d", &dia, &mes, &ano);
+
+    dataValida(dia, mes, ano) ? printf("Dias totais: %d\n", diasTotais(dia, mes, ano)) : printf("Data invalida!\n");
 
     return 0;
 }
