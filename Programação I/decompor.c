31e841deff10a5dd9d7bb53e482f2fd3b2d47e8e
Vini <viniciusicmsc@gmail.com>
2025-06-11 17:06:02 -0300

    C

--- Programação I/decompor.c
+++ Programação I/decompor.c
@@ -2,13 +2,24 @@
 
 char decompor(float x, int *parte_int, float *parte_frac)
 {
-    char sinal = x < 0 ? '-' : '+';
+    char sinal;
 
-    if (x < 0)
-        x *= -1;
+    if (x > 0)
+    {
+        sinal = '+';
+    }
+    else if (x < 0)
+    {
+        x = -x;
+        sinal = '-';
+    }
+    else
+    {
+        sinal = ' ';
+    }
 
-    *parte_frac = x - (int)x;
     *parte_int = (int)x;
+    *parte_frac = x - *parte_int;
 
     return sinal;
 }
@@ -22,20 +33,11 @@ int main()
     printf("Digite um numero: ");
     scanf("%f", &x);
 
-    if (x == 0)
-    {
-        printf("Sinal: \n");
-        printf("Parte inteira: 0\n");
-        printf("Parte fracionaria: 0\n");
-    }
-    else
-    {
-        sinal = decompor(x, &parte_int, &parte_frac);
+    sinal = decompor(x, &parte_int, &parte_frac);
 
-        printf("Sinal: %c\n", sinal);
-        printf("Parte inteira: %d\n", parte_int);
-        printf("Parte fracionaria: %f\n", parte_frac);
-    }
+    printf("Sinal: %c\n", sinal);
+    printf("Parte inteira: %d\n", parte_int);
+    printf("Parte fracionaria: %g\n", parte_frac);
 
     return 0;
 }
